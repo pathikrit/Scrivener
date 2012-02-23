ee242f339c6b2b6425b4c72dab1b0689ccc6a082
Pathikrit Bhowmick <pathikrit@addepar.com>
2012-02-22 23:44:26 -0800

    Forgot to add in previous checkin

--- src/main/java/com/addepar/www/scrivener/S4JExample.java
+++ src/main/java/com/addepar/www/scrivener/S4JExample.java
@@ -1,13 +1,21 @@
 package com.addepar.www.scrivener;
 
+import java.util.UUID;
+
 import static com.addepar.www.scrivener.S4J.*;
 
 public class S4JExample {
 
     public static void main(String[] args) {
         long time = System.currentTimeMillis();
-        S4J.config("adp-rick-test", "http://localhost:8124");
+        S4J.config("adp-rick-test", "http://localhost:8124", "http://github.com/pathikrit/Scrivener/tree/master/src/main/java");
+        warn("hello");
+        warn(UUID.randomUUID().toString());
+        stat("foo", Math.random());
+        stat("foo", Math.random());
+
         stat("call", 1);
+
         warn("test", new RuntimeException());
         foo();
         print(3, "ok");
