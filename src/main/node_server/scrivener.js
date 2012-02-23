cbefb83998c455e3bba6d0c0a80b95d23a6a6955
Pathikrit Bhowmick <pathikrit@addepar.com>
2012-02-23 01:39:41 -0800

    Themes

--- src/main/node_server/scrivener.js
+++ src/main/node_server/scrivener.js
@@ -1,5 +1,5 @@
 "use strict";
-var nano = require('nano')('http://localhost:5984/'), server = require('express').createServer(), fs = require('fs');
+var nano = require('nano')('http://localhost:5984/'), express = require('express'), server = require('express').createServer(), fs = require('fs');
 
 function insert(method, req, res) {
     var appId = req.query.appid, entry = req.query.entry, dbName = appId + '_' + method;
@@ -21,6 +21,6 @@ server.post( '/log', function (req, res) { insert( 'log', req, res); });
 server.post('/stat', function (req, res) { insert('stat', req, res); });
 server.get ( '/log', function (req, res) {   list( 'log', req, res); });
 server.get ('/stat', function (req, res) {   list('stat', req, res); });
-server.get (    '/', function (req, res) { fs.readFile(__dirname + '/index.html', function(err, text) { res.contentType('text/html').send(text); });});
+server.use(express.static(__dirname + '/public'));
 
 server.listen(8124);
